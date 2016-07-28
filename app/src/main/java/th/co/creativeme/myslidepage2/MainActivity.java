a615cf613ff71d76355c6da75461f7748d33fbbc
pingsasin <sasin.ping@gmail.com>
2016-07-28 16:24:03 +0700

    first push

---
+++ app/src/main/java/th/co/creativeme/myslidepage2/MainActivity.java
@@ -0,0 +1,21 @@
+package th.co.creativeme.myslidepage2;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class MainActivity extends TabActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        TabHost mTabHost = getTabHost();
+        mTabHost.addTab(mTabHost.newTabSpec("first").setIndicator("CREDIT CARD").setContent(new Intent(this  ,FirstActivity.class )));
+        mTabHost.addTab(mTabHost.newTabSpec("second").setIndicator("IDENTITY CARD").setContent(new Intent(this , SecondActivity.class )));
+        mTabHost.setCurrentTab(0);
+
+    }
+}
